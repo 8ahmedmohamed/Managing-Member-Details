6caa64f760014d43ed0c288ef0848eb536a3f837
8ahmedmohamed <ahmedkhan.am8@gmail.com>
2023-12-30 23:49:44 +0200

    Editing Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,6 +7,7 @@ const Navbar = () => {
     const LogOut = () => {
         localStorage.removeItem('access_token');
     }
+
     return (
         <React.Fragment>
             <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark border-bottom border-body" data-bs-theme="dark">
@@ -24,10 +25,6 @@ const Navbar = () => {
                                 <Link to='/login' type='button' onClick={LogOut}>Log Out</Link>
                             </li>
                         </ul>
-                        <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form>
                     </div>
                 </div>
             </nav>
